df8561ff8c826e06bcc1d8c385843b6c6719ddfd
Sergey <xxsmotur@gmail.com>
2019-03-31 00:02:04 +0500

    Add Storage package

---
+++ internal/storage/storage.go
@@ -0,0 +1,50 @@
+// Package storage contains handling with db(Postgesql)
+package storage
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/saromanov/gleek/config"
+	"github.com/saromanov/gleek/internal/models"
+)
+
+var (
+	errNoConfig = errors.New("config is not defined")
+	errNoCreds  = errors.New("name, password or user is not defined for storage")
+)
+
+// Storage implements db handling with Postgesql
+type Storage struct {
+	db *gorm.DB
+}
+
+// New provides init for postgesql storage
+func New(s *config.Config) (*Storage, error) {
+	if s == nil {
+		return nil, errNoConfig
+	}
+	if s.Name == "" || s.Password == "" || s.User == "" {
+		return nil, errNoCreds
+	}
+	args := "dbname=gleek"
+	if s.Name != "" && s.Password != "" && s.User != "" {
+		args += fmt.Sprintf(" user=%s dbname=%s password=%s", s.User, s.Name, s.Password)
+	}
+	db, err := gorm.Open("postgres", args)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open db: %v", err)
+	}
+	db.AutoMigrate(&models.Tag{})
+	db.AutoMigrate(&models.Task{})
+	return &Storage{
+		db: db,
+	}, nil
+}
+
+// Close provides closing of db
+func (s *Storage) Close() error {
+	return s.db.Close()
+}
